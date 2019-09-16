8b063ae00e81829cabd5721c003b659c08369056
Kyle <kyle@tahiti-web-management.com>
2019-09-16 10:25:58 -1000

    Méchanique de jeu terminer

--- dragon.py
+++ dragon.py
@@ -14,33 +14,32 @@ class Dragon:
         else:
             print("L'attaque à échoué !!")
             return lp_wizard
-
-    def showStat(self):
-        print("-------Dragon Stats----------")
-        print("Life point " + str(self.life_pt))
-        print("Mana point " + str(self.mana_pt))
-        print("Attack point " + str(self.atk_pt))
-        print("Defense point " + str(self.def_pt))
-        print("Dodge point " + str(self.dodge_pt))
-        print("---------------------------")
-    def dragonChoice(self):
-        return str(random.randint(1,3))
     def dragonDefense(self , attack_wizard):
         if random.randint(1,6) > 4:
             print("Le dragon se défend")
             return self.def_pt - attack_wizard
         else:
-            print("La défense à échoué !!")
-            return attack_wizard
+            print("Le dragon est touché !!")
 
-    def dragonDodge(self):
+            return attack_wizard
+    def dragonDodge(self , wizard_atk):
         print("Le dragon essaye d'esquivé")
-        if random.randint(1, 6) > 4 :
+        if random.randint(1,6) > 4 and self.dodge_pt > 0:
             print("Le dragon à esquivé")
             self.dodge_pt -= 50
-            msg = True
+            return 0
         else:
             print("Le dragon n'a pas esquivé")
-            msg = False
-        self.showStat()
-        return msg
+            return wizard_atk
+    def dragonChoice(self):
+        return str(random.randint(1,3))
+    def showStat(self):
+        print("             -------Dragon Stats-------")
+        print("                  Life point " + str(self.life_pt))
+        print("                  Mana point " + str(self.mana_pt))
+        print("                  Attack point " + str(self.atk_pt))
+        print("                  Defense point " + str(self.def_pt))
+        print("                  Dodge point " + str(self.dodge_pt))
+        print("             --------------------------")
+
+
--- game.py
+++ game.py
@@ -3,35 +3,65 @@ import dragon
 import random
 import sys
 
-w1 = wizard.Wizard(100, 200, 100, 100, 200)
-d1 = dragon.Dragon(100, 200, 100, 100, 200)
+w1 = wizard.Wizard(500, 500, 100, 100, 200,3)
+d1 = dragon.Dragon(500, 200, 100, 100, 200)
 endofWhile = False
 
 w1.showStat()
-d1.showStat()
+# d1.showStat()
 while endofWhile != True:
+    ##Tour du wizard
     response = str(input("Que voulez vous faire ? \n 1 - Attaquer ; 2 - Prendre une potion ; 3 - Lancer un sort"))
     if response == "1":
         if d1.dragonChoice() == "1":
             d1.life_pt = d1.life_pt - d1.dragonDefense(w1.atk_pt)
+            d1.showStat()
         elif d1.dragonChoice() == "2":
-            if d1.dragonDodge():
-                d1.life_pt = w1.wizardAttack(d1.life_pt)
+            d1.life_pt = d1.life_pt - d1.dragonDodge(w1.atk_pt)
+            d1.showStat()
         else:
-            print("Le dragon ne fait rien")
             d1.life_pt = w1.wizardAttack(d1.life_pt)
+            print("Le dragon ne fait rien")
+            d1.showStat()
     elif response == "2":
         w1.takePotion()
+        w1.showStat()
     else:
         w1.launchSpell()
+        w1.showStat()
 
-    w1.life_pt = d1.dragonAttak(w1.life_pt)
-    w1.showStat()
-
-    if d1.life_pt < 0:
+## Vérifie si les deux adversaire sont toujours debout
+    if d1.life_pt <= 0:
         endofWhile = True
         print("You win")
+        break
     elif w1.life_pt < 0 or w1.mana_pt < 0:
         endofWhile = True
         print("You loose")
+        break
 
+
+
+    ##Tour du dragon
+    print("Le dragon attaque")
+    response = str(input("Que voulez vous faire ? \n 1 - Se défendre; 2 - Esquivé; 3 - Prendre une potion"))
+    if response == "1":
+        w1.life_pt = w1.life_pt - w1.wizardDefense(d1.atk_pt)
+        w1.showStat()
+    elif response == "2":
+        w1.life_pt = w1.life_pt - w1.wizardDefense(d1.atk_pt)
+        w1.showStat()
+    else:
+        w1.takePotion()
+        w1.life_pt
+        w1.showStat()
+
+        ## Vérifie si les deux adversaire sont toujours debout
+        if d1.life_pt <= 0:
+            endofWhile = True
+            print("You win")
+            break
+        elif w1.life_pt < 0 or w1.mana_pt < 0:
+            endofWhile = True
+            print("You loose")
+            break
